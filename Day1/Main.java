29f67e4bab6a986d599fd3335e9f31a69f628178
RyonMcAuley <rdmcauley@gmail.com>
2025-12-14 21:11:29 -0500

    add stopwatch

--- Day1/Main.java
+++ Day1/Main.java
@@ -3,51 +3,60 @@ import java.util.Scanner;
 import java.io.IOException;
 
 public class Main {
+    public static int arrow = 50;
+    public static int count = 0;
+
     public static void main(String[] args) {
 
-        int arrow = 50;
-        int count = 0;
+        long start = System.currentTimeMillis();
         try (var scanner = new Scanner(Path.of("code.txt"))) {
             while (scanner.hasNextLine()) {
-                String input = scanner.nextLine();
-                char direction = input.charAt(0);
-                int number = Integer.parseInt(input.substring(1));
-                switch (direction) {
-                    case 'L':
-                        arrow = arrow - number;
-                        while (arrow < 0) {
-                            arrow = arrow + 100;
-                        }
-                        if (arrow == 0)
-                            count++;
-                        break;
-                    case 'R':
-                        arrow = arrow + number;
-                        while (arrow >= 100) {
-                            arrow = arrow - 100;
-                        }
-                        if (arrow == 0)
-                            count++;
-                        break;
-                    default:
-                        System.out.println("Something went wrong...");
-                }
+                handleTurn(scanner.nextLine());
             }
+            long end = System.currentTimeMillis();
             System.out.println("Quick answer: " + count);
+            System.out.println("Time elapsed: " + (end - start));
         } catch (IOException ex) {
             throw new RuntimeException("Error reading file");
         }
 
         // alt approach
+        long start2 = System.currentTimeMillis();
         Safe safe = new Safe();
         try (var scanner = new Scanner(Path.of("code.txt"))) {
             while (scanner.hasNextLine()) {
                 safe.turn(scanner.nextLine());
             }
+            long end2 = System.currentTimeMillis();
+            System.out.println("Object answer: " + safe.getAnswer());
+            System.out.println("Time elapsed: " + (end2 - start2));
         } catch (IOException ex) {
             throw new RuntimeException("Error reading file");
         }
+    }
 
-        System.out.println("Object answer: " + safe.getAnswer());
+    private static void handleTurn(String input) {
+        char direction = input.charAt(0);
+        int number = Integer.parseInt(input.substring(1));
+        switch (direction) {
+            case 'L':
+                arrow = arrow - number;
+                while (arrow < 0) {
+                    arrow = arrow + 100;
+                }
+                if (arrow == 0)
+                    count++;
+                break;
+            case 'R':
+                arrow = arrow + number;
+                while (arrow >= 100) {
+                    arrow = arrow - 100;
+                }
+                if (arrow == 0)
+                    count++;
+                break;
+            default:
+                System.out.println("Something went wrong...");
+        }
     }
 }
